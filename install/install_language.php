2d42d1b2474ac8fdb880ad3071bf9660e2bf5a0b
Stuart Duff <stuartduff76@gmail.com>
2013-05-11 00:42:48 +0100

    made some changes to install launguage file

--- install/install_language.php
+++ install/install_language.php
@@ -6,59 +6,59 @@
  *  Notes: ---
  *  License:
  *
- *   This file is part of Hotaru CMS (http://www.hotarucms.org/).
+ *   This file is part of Baker CMS (http://bakercms.com/).
  *
- *   Hotaru CMS is free software: you can redistribute it and/or modify it under the terms of the
+ *   Baker CMS is free software: you can redistribute it and/or modify it under the terms of the
  *   GNU General Public License as published by the Free Software Foundation, either version 3 of
  *   the License, or (at your option) any later version.
  *
- *   Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ *   Baker CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  *   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *   GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License along with Hotaru CMS. If not,
+ *   You should have received a copy of the GNU General Public License along with Baker CMS. If not,
  *   see http://www.gnu.org/licenses/.
  *
- *   Copyright (c) 2010 Hotaru CMS - http://www.hotarucms.org/
+ *   Copyright (c) 2010 Baker CMS - http://bakercms.com/
  *
  **************************************************************************************************** */
 
 /* Upgrade Step 1 */
-$lang["upgrade_title"] = "Hotaru CMS Upgrade";
+$lang["upgrade_title"] = "Baker CMS Upgrade";
 
 $lang["upgrade_step1"] = "Step 1/3: Checking your existing setup";
-$lang["upgrade_step1_details"] = "To upgrade Hotaru to version " . $h->version . ", click 'Next'...";
-$lang["upgrade_step1_old_version"] = "You are currently running Hotaru CMS version ";
-$lang["upgrade_step1_old_no_version"] = "We could not find an existing version number of Hotaru CMS in your database.";
-$lang["upgrade_step1_current_version"] = "You already have the latest version of Hotaru installed.<br/><br/>If you want to run the upgrade script again click 'Next', otherwise you can close this browser window now.";
+$lang["upgrade_step1_details"] = "To upgrade Baker CMS to version " . $h->version . ", click 'Next'...";
+$lang["upgrade_step1_old_version"] = "You are currently running Baker CMS version ";
+$lang["upgrade_step1_old_no_version"] = "We could not find an existing version number of Baker CMSS in your database.";
+$lang["upgrade_step1_current_version"] = "You already have the latest version of Baker CMS installed.<br/><br/>If you want to run the upgrade script again click 'Next', otherwise you can close this browser window now.";
 
 /* Upgrade Step 2 */
 $lang["upgrade_step2"] = "Step 2/3: Upgrade Complete";
-$lang["upgrade_step2_details"] = "Congratulations! You have successfully upgraded Hotaru CMS.";
+$lang["upgrade_step2_details"] = "Congratulations! You have successfully upgraded Baker CMS.";
 
 /* Upgrade Step 2 */
 $lang["upgrade_step3"] = "Step 3/3: Check Plugins, Templates";
-$lang["upgrade_step3_details"] = "You have successfully upgraded Hotaru CMS.";
-$lang["upgrade_step3_instructions"] = "After clicking \"Finish\" you may find some of your plugins need upgrading. You can check the latest version numbers from the Plugin Management page in your admin dashboard. You may also need to modify any templates you have customised to make sure they work with the latest version of Hotaru CMS.";
-$lang["upgrade_step3_go_play"] = "Click \"Finish\" to access your Hotaru site!";
+$lang["upgrade_step3_details"] = "You have successfully upgraded Baker CMS.";
+$lang["upgrade_step3_instructions"] = "After clicking \"Finish\" you may find some of your plugins need upgrading. You can check the latest version numbers from the Plugin Management page in your admin dashboard. You may also need to modify any templates you have customised to make sure they work with the latest version of Baker CMS.";
+$lang["upgrade_step3_go_play"] = "Click \"Finish\" to access your Baker CMS site!";
 $lang["upgrade_home"] = "Finish";
 
 /* Install Common */
-$lang["install_title"] = "Hotaru CMS Setup";
+$lang["install_title"] = "Baker CMS Setup";
 $lang["install_next"] = "Next";
 $lang["install_back"] = "Back";
-$lang["install_trouble"] = "Having trouble? Read the <a href='http://docs.hotarucms.org'>Documentation</a> or ask for help in the <a href='http://hotarucms.org/forum.php'>Forums</a>.";
+$lang["install_trouble"] = "Having trouble? Read the <a href='http://bakercms.com/codex-docs/'>Documentation</a> or ask for help in the <a href='http://bakercms.com/forum/index.php'>Forums</a>.";
 
 /* Install Step 1 */
-$lang["install_step0"] = "Welcome to Hotaru CMS";
-$lang["install_step0_welcome"] = "Installing Hotaru can be completed in just 4 steps and normally requires just a few minutes.";
-$lang["install_step0_select"] = "Select below whether you want to make a new installation or upgrade an existing Hotaru site...";
+$lang["install_step0"] = "Welcome to Baker CMS";
+$lang["install_step0_welcome"] = "Installing Baker CMS can be completed in just 4 steps and normally requires just a few minutes.";
+$lang["install_step0_select"] = "Select below whether you want to make a new installation or upgrade an existing Baker CMS site...";
 $lang["install_new"] = "New Installation";
 $lang["install_upgrade"] = "Upgrade Existing Site";
 
 /* Install Step 2 */
 $lang["install_step1"] = "Step 1/4: Database Setup";
-$lang["install_step1_instructions"] = "To set up a database for Hotaru CMS, you'll need to do the following";
+$lang["install_step1_instructions"] = "To set up a database for Baker CMS, you'll need to do the following";
 $lang["install_step1_instructions1"] = "Create a database called <i>hotaru</i> in your web host's control panel. Make a note of your username and password!";
 $lang["install_step1_instructions2"] = "Copy <pre>/hotaru_settings_default.php</pre> and rename it <pre>/hotaru_settings.php</pre>.";
 $lang["install_step1_instructions3"] = "Open <pre>/hotaru_settings.php</pre> and fill in the \"Database Details\" section.";
@@ -79,7 +79,7 @@ $lang["install_step1_dbpassword_explain"] = "";
 $lang["install_step1_dbname"] = "<b>Database Name</b>";
 $lang["install_step1_dbname_explain"] = "";
 $lang["install_step1_dbprefix"] = "<b>Database Prefix</b>";
-$lang["install_step1_dbprefix_explain"] = "Database prefix, e.g. 'hotaru_'";
+$lang["install_step1_dbprefix_explain"] = "Database prefix, e.g. 'bakercms_'";
 $lang["install_step1_dbhost"] = "<b>Database Host</b>";
 $lang["install_step1_dbhost_explain"] = "You probably won't need to change this";
 
@@ -90,8 +90,8 @@ $lang["install_step1_dbname_error"] = "There was a problem with the database nam
 $lang["install_step1_dbprefix_error"] = "There was a problem with the database prefix";
 $lang["install_step1_dbhost_error"] = "There was a problem with the database host";
 
-$lang["install_step1_settings_file_already_exists"] = "There is already a Hotaru settings file on your server. If you press 'update' your existing file will be updated with the settings above.";
-$lang["install_step1_settings_db_already_exists"] = "There is already a Hotaru database on your server with live tables. If you press 'Next' your entire database will be wiped clean. Proceed with caution.";
+$lang["install_step1_settings_file_already_exists"] = "There is already a Baker CMS settings file on your server. If you press 'update' your existing file will be updated with the settings above.";
+$lang["install_step1_settings_db_already_exists"] = "There is already a Baker CMS database on your server with live tables. If you press 'Next' your entire database will be wiped clean. Proceed with caution.";
 $lang["install_step1_update_file_writing_success"] = "The 'hotaru_settings' file was created.";
 $lang["install_step1_update_file_writing_failure"] = "There was a problem creating the 'hotaru_settings' file.";
 $lang["install_step1_no_db_exists_failure"] = "The database does not exist or the connection settings are incorrect.";
@@ -104,13 +104,13 @@ $lang["install_step2_no_tables"] = "No existing tables were found in the databas
 $lang["install_step2_creating_table"] = "Creating table";
 $lang["install_step2_adding_data"] = "Adding data to";
 $lang["install_step2_deleting_table"] = "Deleting existing tables";
-$lang["install_step2_already_exists"] = "It seems there are already tables for Hotaru CMS in the database.";
+$lang["install_step2_already_exists"] = "It seems there are already tables for Baker CMS in the database.";
 $lang["install_step2_continue"] = "Click \"Next\" to continue.";
 $lang["install_step2_rebuild_note"] = "<i>Note</i>: If you'd like to start fresh, ";
 $lang["install_step2_rebuild_link"] = "delete and rebuild the database tables";
-$lang["install_step2_success"] = "Database tables created successfully. Click \"Next\" to configure Hotaru CMS.";
+$lang["install_step2_success"] = "Database tables created successfully. Click \"Next\" to configureBaker CMS.";
 $lang["install_step2_fail"] = "There were some errors in creating database tables. Not all tables may have been created correctly.";
-$lang["install_step2_existing_db"] = "You already have an existing installation of Hotaru CMS.<br/><br/>If you continue, this installation will DELETE all your existing tables and settings, including posts, users and plugin data.";
+$lang["install_step2_existing_db"] = "You already have an existing installation of Baker CMS.<br/><br/>If you continue, this installation will DELETE all your existing tables and settings, including posts, users and plugin data.";
 $lang["install_step2_existing_confirm"] = "Confirm you wish to continue this install by typing 'DELETE' in the box and press the button";
 $lang["install_step2_existing_go_upgrade1"] = "Alternatively, you may wish to ";
 $lang["install_step2_existing_go_upgrade2"] = "run the upgrade script";
@@ -143,10 +143,10 @@ $lang["install_step4_installation_delete"] = "<span style='color: red;'><b>WARNI
 $lang["install_step4_form_check_php"] = "Check PHP Setup";
 $lang["install_step4_form_check_php_warning"] = "Note: Your server is missing the PHP module: ";
 $lang["install_step4_form_check_php_success"] = "Your server has the required PHP modules";
-$lang["install_step4_form_check_php_version"] = "Hotaru has not been tested on this version of PHP. You may need to upgrade";
+$lang["install_step4_form_check_php_version"] = "Baker CMS has not been tested on this version of PHP. You may need to upgrade";
 
 
-$lang["install_step4_installation_go_play"] = "Done? Okay, go and play with your new Hotaru site!";
+$lang["install_step4_installation_go_play"] = "Done? Okay, go and play with your new Baker CMS site!";
 $lang["install_home"] = "Get Started!";
 
 ?>
